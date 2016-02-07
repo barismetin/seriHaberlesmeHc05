7496b23c43c1d20c83402ab09f9ef8199ebe118b
barisMetin <me@barismetin.com>
2016-02-07 22:21:21 +0200

    Boşluklar düzeltildi

--- main.c
+++ main.c
@@ -12,69 +12,67 @@ char buff[100];
 void init_USART1(uint32_t baudrate){
 
 
-GPIO_InitTypeDef GPIO_InitStruct;
-USART_InitTypeDef USART_InitStruct;
-NVIC_InitTypeDef NVIC_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStruct;
+	USART_InitTypeDef USART_InitStruct;
+	NVIC_InitTypeDef NVIC_InitStructure;
 
-RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD,ENABLE);
-GPIOD->MODER = 0x55000000;     // GPIOD nin 15, 14, 13, 12 pinleri cikis tanimlandi (LEDler icin)
-GPIOD->OSPEEDR= 0xFFFFFFFF;
-RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-
-
-RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD,ENABLE);
+	GPIOD->MODER = 0x55000000;     // GPIOD nin 15, 14, 13, 12 pinleri cikis tanimlandi (LEDler icin)
+	GPIOD->OSPEEDR= 0xFFFFFFFF;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
 
 
-GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7; // Pins B6 (TX) and B7 (RX)
-GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-GPIO_Init(GPIOB, &GPIO_InitStruct);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 
 
-GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_USART1);
-GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_USART1);
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7; // Pins B6 (TX) and B7 (RX)
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 
-USART_InitStruct.USART_BaudRate = 9600;
-USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-USART_InitStruct.USART_StopBits = USART_StopBits_1;
-USART_InitStruct.USART_Parity = USART_Parity_No;
-USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
-USART_Init(USART1, &USART_InitStruct);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_USART1);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_USART1);
+
+
+	USART_InitStruct.USART_BaudRate = 9600;
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+	USART_InitStruct.USART_Parity = USART_Parity_No;
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+	USART_Init(USART1, &USART_InitStruct);
 
 
 
 
-USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
 
-NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-NVIC_Init(&NVIC_InitStructure);
-
-
-USART_Cmd(USART1, ENABLE);
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	VIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+	USART_Cmd(USART1, ENABLE);
 }
 
 
 void USART_puts(USART_TypeDef* USARTx, volatile char *s){
 
-while(*s){
+	while(*s){
 
-while( !(USARTx->SR & 0x00000040) );
-USART_SendData(USARTx, *s);
-*s++;
-}
-}
+		while( !(USARTx->SR & 0x00000040) );
+			USART_SendData(USARTx, *s);
+			*s++;
+			}
+	}
 void send_char(char c)
 {
 	while(!(USART1->SR&0x80));
 	   USART1->DR=c;
-}
+	}
 void USART1_IRQHandler(void)
   {
     if( USART_GetITStatus(USART1, USART_IT_RXNE) )
